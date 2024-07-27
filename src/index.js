b710b798d00d1d983304b71bf0919def87a7abb2
Ben Penwell <benpenwell@gmail.com>
2024-07-26 17:02:48 -0700

    Fix css issues

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { App } from './App';
-import { createRoot } from 'react-dom/client';
-var container = document.getElementById('root');
-var root = createRoot(container);
-root.render(_jsx(App, {}));
-reportWebVitals();
