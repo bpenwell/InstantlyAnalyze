2711d7accdf9071417663250e6ef8e5c82ba68aa
Ben Penwell <benpenwell@gmail.com>
2024-12-28 16:32:26 -0500

    Remove stripe server details

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const stripe = require('stripe')('sk_test_51Qa9uwKDFjdgEMB7hoam2Tswsgy25OVPlhL7X4uoI58JylfQ6IsXcUSRo8aU1jw39oR0duCdCNJ55gGMIrptSehd00XItzal7e');
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -16,21 +15,4 @@ app.get('*', (req, res) => {
 
 app.listen(PORT, () => {
   console.debug(`Server is running on port ${PORT}`);
-});
-
-app.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: 'price_1Qa9oyQ5DPPArad12tf4aezM',
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: `${YOUR_DOMAIN}?success=true`,
-    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
-});
-
-  res.redirect(303, session.url);
 });
