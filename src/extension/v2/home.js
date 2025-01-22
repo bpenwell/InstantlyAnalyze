caecc4bb9fdd51eeaeac3469ccbe64822622ce6e
Ben Penwell <benpenwell@gmail.com>
2025-01-22 12:47:10 -0800

    remove extension

--- src/extension/v2/home.js
+++
@@ -1,8 +0,0 @@
-chrome.storage.sync.get(['allPages', 'pageCount', 'interval'], (result) => {
-  if (result.allPages) {
-    console.log('All pages selected');
-  } else {
-    console.log(`Page count: ${result.pageCount}`);
-  }
-  console.log(`Interval: ${result.interval} seconds`);
-});
